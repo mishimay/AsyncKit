e2d75d7459bfbdb0885a26d0ccd74c2f626fcc35
Suguru Kishimoto <melodydance.k.s@gmail.com>
2016-01-04 09:56:53 +0900

    Add xcodeproj for Carthage

---
+++ Pod/Classes/AsyncKit.h
@@ -0,0 +1,16 @@
+//
+//  AsyncKit.h
+//  AsyncKit
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncKit.
+FOUNDATION_EXPORT double AsyncKitVersionNumber;
+
+//! Project version string for AsyncKit.
+FOUNDATION_EXPORT const unsigned char AsyncKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncKit/PublicHeader.h>
+
+
